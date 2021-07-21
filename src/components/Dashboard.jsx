e578c370ae68f3fc0af6677f2bb22714bcadb3b6
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-07-20 22:15:29 -0500

    cambios en el dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import DashboardCard from "./DashboardCard";
+import "./Dashboard.css";
 
 const Dashboard = (props) => {
   return (
     <div className="dashboard">
+      <DashboardCard></DashboardCard>
+      <DashboardCard></DashboardCard>
+      <DashboardCard></DashboardCard>
+      <DashboardCard></DashboardCard>
+      <DashboardCard></DashboardCard>
       <DashboardCard></DashboardCard>
     </div>
   );
