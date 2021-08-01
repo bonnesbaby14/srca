dc368cd66847cfa04a04bf50cb9b1dfee18be87b
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-08-01 16:09:58 -0500

    recoleccion de datos del cliente

--- src/components/ModalClient.jsx
+++ src/components/ModalClient.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./ModalClient.css";
 
 const ModalClient = ({ closeModal }) => {
+  const [data, setdata] = useState({
+    name: "",
+    phone: "",
+    mail: "",
+    website: "",
+  });
   const handleModal = (event) => {
-    event.preventDefault();
+    const datafinal = { ...data };
+    datafinal[event.target.id] = event.target.value;
+
+    setdata(datafinal);
   };
+
   const handleCloseModal = () => {
     closeModal(false);
+  };
+  const handleUpProject = (e) => {
+    e.preventDefault();
+    console.log(data);
   };
   return (
     <div className="modal">
       <form className="modalForm" onSubmit={handleModal}>
         <h2>Agregar cliente</h2>
-        <input type="text" placeholder="Nombre" />
-        <input type="text" placeholder="Telefono" />
-        <input type="text" placeholder="Correo" />
-        <input type="text" placeholder="WebSite" />
-        <button onClick={() => {}}>Guardar</button>
+        <input
+          id="name"
+          onChange={handleModal}
+          type="text"
+          placeholder="Nombre"
+        />
+        <input
+          id="phone"
+          onChange={handleModal}
+          type="text"
+          placeholder="Telefono"
+        />
+        <input
+          id="mail"
+          onChange={handleModal}
+          type="text"
+          placeholder="Correo"
+        />
+        <input
+          id="website"
+          onChange={handleModal}
+          type="text"
+          placeholder="WebSite"
+        />
+        <button onClick={handleUpProject}>Guardar</button>
         <button onClick={handleCloseModal}>Cancelar</button>
       </form>
     </div>
