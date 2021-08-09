b409ea2c39a03d15b68d6c6337008a6eb5b8fc10
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-08-08 19:20:05 -0500

    intento de un escaner

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Scanner from "./Scanner";
 
 import "./Settings.css";
 
 const Settings = (props) => {
-  return <div className="settings"></div>;
+  const [camera, setCamera] = useState(false);
+  const [result, setResult] = useState(null);
+  const onDetected = (result) => {
+    setResult(result);
+  };
+
+  return (
+    <div className="settings">
+      <div style={{ border: "solid red 1px" }}>
+        <p>{result ? result : "Scanning..."}</p>
+        <button onClick={() => setCamera(!camera)}>
+          {camera ? "Stop" : "Start"}
+        </button>
+        <div className="container">
+          {camera && <Scanner onDetected={onDetected} />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
