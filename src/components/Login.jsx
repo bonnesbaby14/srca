7ce5fdcdab1afa7999ec1c169bf8b244634729e4
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-08-14 20:06:17 -0500

    cambios en el login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,53 +23,22 @@ const Login = (props) => {
 
   return (
     <div className="login">
-      <div>
-        <div>
-          <span>
-            <h2>
-              <span />
-            </h2>
-          </span>
-          <h4>Your Company Logo</h4>
+      <div className="main">
+        <div className="title">
+          <h1>Login</h1>
         </div>
-        <div>
-          <div>
-            <div>
-              <h2>Log In</h2>
-            </div>
-            <div>
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    placeholder="Username"
-                  />
-                </div>
-                <div>
-                  {/* <span class="fa fa-lock"></span> */}
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                  />
-                </div>
-                <div>
-                  <input type="checkbox" name="remember_me" id="remember_me" />
-                  <label htmlFor="remember_me">Remember Me!</label>
-                </div>
-                <div>
-                  <input type="submit" defaultValue="Submit" />
-                </div>
-              </form>
-            </div>
-            <div>
-              <p>Don't have an account?</p>
-            </div>
+        <form action="#" className="form">
+          <div className="user">
+            <input type="text" placeholder="Usuario" />
           </div>
-        </div>
+          <div className="password">
+            <input type="password" placeholder="Contraseña" />
+          </div>
+
+          <div className="boton">
+            <button onClick={handleSubmit}>Inicia Sesión</button>
+          </div>
+        </form>
       </div>
     </div>
   );
