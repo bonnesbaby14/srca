69cfcbe5bc517635d79b63e8b9fbd07ee29d9e81
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-08-22 19:02:45 -0500

    cambios en el dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-
+import React, { useEffect, useState, useContext } from "react";
 import DashboardCard from "./DashboardCard";
+import { UserContext } from "../context/contexts";
 import { Line } from "react-chartjs-2";
 import "./Dashboard.css";
 
@@ -8,6 +8,9 @@ import { FaTicketAlt, FaUser, FaPager } from "react-icons/fa";
 import { FaSistrix } from "react-icons/fa";
 
 const Dashboard = (props) => {
+  const { log, setUser } = useContext(UserContext);
+
+  const [isLoading, setIsLoading] = useState(false);
   const data = {
     labels: [
       "Enero",
@@ -35,6 +38,7 @@ const Dashboard = (props) => {
   };
 
   const options = {
+    responsive: true,
     scales: {
       yAxes: [
         {
@@ -45,6 +49,36 @@ const Dashboard = (props) => {
       ],
     },
   };
+  const getData = () => {
+    setIsLoading(true);
+    fetch("https://srcaapi.gabrielangeles.com/clients", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: log.authKey,
+        // 'Content-Type': 'application/json'
+      },
+    }).then(
+      (data) => {
+        if (data.status === 401) {
+          setUser({ type: "logout" });
+        } else {
+          data.json().then((data) => {
+            setIsLoading(false);
+          });
+        }
+      },
+      (err) => {
+        console.log(err);
+        setIsLoading(false);
+      }
+    );
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="dashboard">
       <div className="first">
